cbf85b94e7bf52387f4b025122ed87b59f5abcbe
firmread <litchirhythm@gmail.com>
2016-02-21 16:58:02 -0500

    focus on example that should be here first

---
+++ 07_hardware/02_DMX/src/ofApp.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxDmx.h"
+#include "ofxGui.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+        ofxDmx dmx;
+        int level;
+        
+        ofxPanel panel;
+        ofParameter<int> chan1;
+        ofParameter<int> chan2;
+        ofParameter<int> chan3;
+        ofParameter<bool> autoCycle;
+		
+};
