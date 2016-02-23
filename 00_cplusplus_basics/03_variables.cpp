c61b6a29f01c2e4d3bfe993191297babf30b067d
firmread <litchirhythm@gmail.com>
2016-02-23 12:41:00 -0500

    indent unsave saved

--- 00_cplusplus_basics/03_variables.cpp
+++ 00_cplusplus_basics/03_variables.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(){
 
-int answer = 42;
+	int answer = 42;
 
-cout << "My friend is " << answer << " years old." << endl;
-cout << "The answer to the life the universe and everything is " << answer << "." << endl;
-cout << "That number plus 1 is " << (answer+1) << "." << endl;
-return 0;
+	cout << "My friend is " << answer << " years old." << endl;
+	cout << "The answer to the life the universe and everything is " << answer << "." << endl;
+	cout << "That number plus 1 is " << (answer+1) << "." << endl;
+	return 0;
 }
